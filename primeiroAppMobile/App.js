b8b97eddd53bfe58291d5436952074140659fa3f
Otávio Augusto <60992292+Otaviodias21@users.noreply.github.com>
2023-02-25 16:57:22 -0300

    Update App.js

--- primeiroAppMobile/App.js
+++ primeiroAppMobile/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, Text, Image} from 'react-native';
-import { Paragraph } from 'react-native-paper';
 
 class App extends Component{
   render(){
